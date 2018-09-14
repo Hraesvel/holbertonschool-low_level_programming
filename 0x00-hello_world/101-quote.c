891271f8b075665e159d8431e81bea8150dfdb0c
Martin smith <538@holbertonschool.com>
2018-09-14 19:41:45 +0000

    task 9 hotfix
    use write rather than fprintf() since requirement request not to use that function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <errno.h>
+#include <unistd.h>
 #include <string.h>
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 	return (1);
 }
