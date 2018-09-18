e580f40ca82d23c5e9e14b7457391c133075c552
Martin smith <538@holbertonschool.com>
2018-09-18 17:27:49 +0000

    modify task 1
    fix styling and remove  since its a void

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 /**
  * print_alphabet - wirte the the lower-case alphabet on a line.
- * 
+ *
  *
  *
  */
 void print_alphabet(void)
 {
-        char c;
+	char c;
 
-	for ( c = 'a' ; c <= 'z' ; c++)
+	for (c = 'a' ; c <= 'z' ; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-        return;
 }
