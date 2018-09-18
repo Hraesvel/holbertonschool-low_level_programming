03a6ce118e89f9bb28ba3eabe42f8136665f368d
Martin smith <538@holbertonschool.com>
2018-09-18 18:10:45 +0000

    fix docs

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <ctype.h>
 /**
- * _islower - check if the given character is lower case.
+ * _isalpha - check if the given character is lower case.
  * @c: character to test.
  *
  * Return: 1 (True) else 0 (False)
