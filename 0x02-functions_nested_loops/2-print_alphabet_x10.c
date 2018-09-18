cc3610e86e5b1afc34c7471627c039f50861858e
Martin smith <538@holbertonschool.com>
2018-09-18 18:05:24 +0000

    modify task 2
    fix docs, to discribe the right funtion

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,8 @@
 #include "holberton.h"
+
 /**
- * print_alphabet - wirte the the lower-case alphabet on a line.
+ * print_alphabet_x10 - write the the lower-case alphabet on a line.
+ *
  */
 void print_alphabet_x10(void)
 {
