08a508af0007d2ee12a81dbd48460cff86d26fb7
Martin smith <538@holbertonschool.com>
2018-09-18 19:26:20 +0000

    Modify task 12: file 100-times_tables.c
    move the If statment into their own functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,88 @@
 #include "holberton.h"
 
 /**
+ * print_thousand - print out the digit in the thousand spot
+ * @sum: Value to scan for thousand spot
+ *
+ * Return: Void
+ */
+static void print_thousand(int sum)
+{
+	if (sum / 1000 == 0)
+	{
+		_putchar(32);
+	}
+	else
+	{
+		_putchar('0' + (sum / 1000));
+	}
+}
+
+
+/**
+ * print_hundred - print out the digit in the hundred spot
+ * @sum: Value to scan for hundred spot
+ *
+ * Return: Void
+ */
+static void print_hundred(int sum)
+{
+	if (sum / 100 == 0)
+	{
+		_putchar(32);
+	}
+	else
+	{
+		_putchar('0' + (sum / 100));
+	}
+}
+
+
+/**
+ * print_tenth - print out the digit in the tenth spot
+ * @sum: Value to scan for tenth spot
+ *
+ * Return: Void
+ */
+static void print_tenth(int sum)
+{
+	if (sum / 10 == 0)
+	{
+		_putchar(32);
+	}
+	else
+	{
+		if ((sum / 10) > 9)
+		{
+			_putchar('0' + (sum / 10 % 10));
+		}
+		else
+		{
+			_putchar('0' + (sum / 10));
+		}
+	}
+}
+
+
+/**
+ * print_base10 - print out the digit in the base10 spot
+ * @sum: Value to scan for base10 spot
+ *
+ * Return: Void
+ */
+static void print_base10(int sum)
+{
+	if (sum % 10 == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		_putchar('0' + (sum % 10));
+	}
+}
+
+/**
  * print_times_table - print out the 9 times table
  * @n: Valuse use to generate times tables.
  *
@@ -22,45 +104,10 @@ void print_times_table(int n)
 			int sum = v * h;
 
 			_putchar(',');
-			if (sum / 1000 == 0)
-			{
-				_putchar(32);
-			}
-			else
-			{
-				_putchar('0' + (sum / 1000));
-			}
-			if (sum / 100 == 0)
-			{
-				_putchar(32);
-			}
-			else
-			{
-				_putchar('0' + (sum / 100));
-			}
-			if (sum / 10 == 0)
-			{
-				_putchar(32);
-			}
-			else
-			{
-				if ((sum / 10) > 9)
-				{
-					_putchar('0' + (sum / 10 % 10));
-				}
-				else
-				{
-				_putchar('0' + (sum / 10));
-				}
-			}
-			if (sum % 10 == 0)
-			{
-				_putchar('0');
-			}
-			else
-			{
-				_putchar('0' + (sum % 10));
-			}
+			print_thousand(sum);
+			print_hundred(sum);
+			print_tenth(sum);
+			print_base10(sum);
 		}
 		_putchar('\n');
 	}
