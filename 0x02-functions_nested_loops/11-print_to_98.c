f5c2748f1a2ca4115107d1e2e303d71acb4c256e
Martin smith <538@holbertonschool.com>
2018-09-18 18:46:54 +0000

    modify task 11
    move when the  is printed, now happens when the comma is printed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
  * print_to_98 - count the natural numbers from n to 98.
  * @n: value to count from.
@@ -17,18 +18,18 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf(" %d", i);
+			printf("%d", i);
 			if (i < 98)
-				printf(",");
+				printf(", ");
 		}
 	}
 	else if (n > 98)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf(" %d", i);
+			printf("%d", i);
 			if (i > 98)
-				printf(",");
+				printf(", ");
 
 		}
 	}
