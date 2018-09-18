9097c483a16372ad3081a60240a2dbcf21275033
Martin smith <538@holbertonschool.com>
2018-09-18 02:21:10 +0000

    answer for 0-10
    adding the answer for task 0-10

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * _abs - return the absolute value of a given integer.
+ * @i: value use to find the absolute value.
+ *
+ *
+ * Return: the absolute value.
+ */
+int _abs(int i)
+{
+	return (abs(i));
+}
