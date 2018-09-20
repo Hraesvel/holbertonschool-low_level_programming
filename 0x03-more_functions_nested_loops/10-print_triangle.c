dfa8e33708fe0263eb1f723c87ab3be2d61fd3a7
Martin smith <538@holbertonschool.com>
2018-09-20 19:18:58 +0000

    cleanup

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -8,16 +8,27 @@
  */
 void print_triangle(int size)
 {
-
+/**
+ * int i: for iteration in for loop also use as a way to subtract.
+ *
+ * int margin: will recored the amount of whitesapce ill need to print.
+ * int dis"tance": is used to offset size.
+ * int to_print: is used to tell how many '#' will need to be print per-line
+ */
 	int i, margin, dis, to_print;
 
 	dis = size-1;
 	for (i = 0 ; i < size ; i++)
 	{
+/**
+ * we set the margin to Zero every iteration. since we're try to only print
+ * the right amount of white space. we do some math to dis to set the stopping
+ * point
+ */
 		margin = 0;
-		while ( margin < (dis - i))
+		while (margin < (dis - i))
 		{
-			_putchar('.');
+			_putchar(32);
 			margin++;
 		}
 
