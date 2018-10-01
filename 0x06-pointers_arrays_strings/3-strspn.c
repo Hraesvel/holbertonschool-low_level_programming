55cce7de3d6f98f761a31c690014127a12cad845
Martin Smith <538@holbertonschoo.com>
2018-10-01 21:42:50 +0000

    Modify 3-strspn

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -10,23 +10,28 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, z, count;
+	unsigned int i, z, count, checker;
 
 	i = 0;
 	z = 0;
+	checker = 0;
 	count = 0;
-	while (s[i] != ',')
+	while (s[i] != '\0')
 	{
 		z = 0;
+		checker = 0;
 		while (accept[z] != '\0')
 		{
 			if (accept[z] == s[i])
 			{
 				count++;
+				checker = 1;
 			}
 			z++;
 		}
 
+		if (checker == 0)
+			return (count);
 		i++;
 	}
 	return (count);
