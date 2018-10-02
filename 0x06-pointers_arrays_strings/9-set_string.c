558264889a7838a9f1805c346daabf368917c4b2
Martin smith <538@holbertonschool.com>
2018-10-02 02:58:11 +0000

    adding task 9

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - desciption
+ * @s: source for copy
+ * @to: string to copy to.
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
