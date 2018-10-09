75e1e34449666716f4fb1431f0122d86d7bd04db
Martin smith <538@holbertonschool.com>
2018-10-09 21:22:56 +0000

    hotfix
    check for null rather then null byte

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -12,11 +12,11 @@ char *_strdup(char *str)
 	int i;
 	char *s;
 
-	if (!(*str))
+	if (str == 0)
 		return (0);
 	for (i = 0; str[i]; i++)
 		;
-	s = malloc(sizeof(char) * i + 1);
+	s = malloc(sizeof(char) * (i + 1));
 	if (s == 0)
 		return (0);
 	s[i] = '\0';
