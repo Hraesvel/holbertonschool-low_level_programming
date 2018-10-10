811f99bf35810e3413377e1b156e81702ecc4d8f
Martin smith <538@holbertonschool.com>
2018-10-10 00:43:21 +0000

    hotfix

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -17,12 +17,12 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = (int **)malloc(sizeof(*arr) * height);
+	arr = malloc(sizeof(*arr) * height);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < width; i++)
 	{
-		arr[i] = (int *)malloc(sizeof(int) * width);
+		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
 			while (i--)
