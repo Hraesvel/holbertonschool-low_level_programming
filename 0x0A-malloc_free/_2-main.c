39e372037e2ee8adaec5ea7da460a7841e07ab3f
Martin smith <538@holbertonschool.com>
2018-10-10 01:06:46 +0000

    revert 3-alloc_grid
    revert to passing version

--- 0x0A-malloc_free/_2-main.c
+++ 0x0A-malloc_free/_2-main.c
@@ -7,12 +7,20 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int main(int ac, char *av[])
 {
 	char *s;
 	
-	s = str_concat(NULL, NULL);
-	
+	if (ac <= 2)
+		s = str_concat("Betty ", "Holberton");
+	else if (ac > 3)
+	{
+		printf("only 2\n");
+		return (1);
+	}
+	else
+		s = str_concat(av[1], av[2]);
+
 	if (s == NULL)
 	{
 		printf("failed\n");
