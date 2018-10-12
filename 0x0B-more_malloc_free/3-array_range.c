ea65b4249f6da5e86a46d8a94d4b24f78fb8944a
Martin smith <538@holbertonschool.com>
2018-10-12 00:26:31 +0000

    Modify 3
    add logic for negitives

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * array_range - Function create and array of integers.
  * @min: starting number.
@@ -11,15 +11,29 @@
 int *array_range(int min, int max)
 {
 	int *arr;
+	int a, b;
 
+	a = b = 0;
 	if (min > max)
 		return (NULL);
-	arr = malloc(sizeof(int) * (max - min + 1));
+
+	if (min < 0)
+	{
+		a = abs(min);
+		b = abs(max);
+
+		b = b + a;
+	}
+	else
+		b = max - min;
+
+	arr = malloc(sizeof(int) * (b + 1));
 	if (!arr)
 		return (NULL);
-	do
-		arr[max] = max;
-	while (--max >= min);
+	do {
+		arr[b] = max;
+		max--;
+	} while (--b >= 0);
 
 	return (arr);
 }
