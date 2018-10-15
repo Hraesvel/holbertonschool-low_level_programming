c09fa6c3aefe0550ff05f18e4d787a108cdbce9a
Martin smith <538@holbertonschool.com>
2018-10-15 20:31:57 +0000

    hotfix 4-new_dog
    check if name or owner sooner

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -13,20 +13,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
+	if (!name || !owner)
+		return (NULL);
+
 	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
-	if (name)
-		d->name = name;
-	else
-		return (NULL);
 
+	d->name = name;
 	d->age = age;
-
-	if (owner)
-		d->owner = owner;
-	else
-		return (NULL);
+	d->owner = owner;
 
 	return (d);
 }
