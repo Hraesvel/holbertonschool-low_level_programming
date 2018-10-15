66574690a697bd8f8b796fcd6e2b183176d9a6ae
Martin smith <538@holbertonschool.com>
2018-10-15 22:01:25 +0000

    modify 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -3,6 +3,20 @@
 #include <string.h>
 
 /**
+ * _strcopy - copy read only data to mutatable.
+ * @dst: pointer to copy char to.
+ * @src: read only data.
+ */
+void _strcopy(char *dst, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dst[i] = src[i];
+	dst[i] = '\0';
+}
+
+/**
  * new_dog - create new dog from the dna of the first dog.
  * @name: name of dog
  * @age: age of dog
@@ -13,29 +27,31 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-
+	char *n, *o;
 
 	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
 
-	d->name = malloc(sizeof(name));
-	if (!(d->name))
+	n = malloc(sizeof(name));
+	if (!n)
 	{
 		free(d);
 		return (NULL);
 	}
-	strcpy(d->name , name);
 
-	d->owner = malloc(sizeof(owner));
-	if (!(d->owner))
+	o = malloc(sizeof(owner));
+	if (!o)
 	{
-		free(d->name);
+		free(n);
 		free(d);
 		return (NULL);
 	}
-	strcpy(d->owner, owner);
+
+	_strcopy(n, name);
+	_strcopy(o, owner);
+	d->name = n;
 	d->age = age;
-
+	d->owner = o;
 	return (d);
 }
