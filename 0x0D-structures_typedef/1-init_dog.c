a4ad28304450cdabc5eaf4e77878da9a6429a4e7
Martin smith <538@holbertonschool.com>
2018-10-15 20:10:59 +0000

    hotfix 1-init_dog
    logic add if pointer 'dog' is null

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - Initialized dog stuct.
@@ -11,6 +12,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!d)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
