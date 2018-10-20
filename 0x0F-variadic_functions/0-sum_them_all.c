72ec0f562b96c05cf66c625aad94a61dc0446546
Martin Smith <538@holbertonschool.com>
2018-10-20 21:49:34 +0000

    modify task 0
    change for loop logic  to

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -16,7 +16,7 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	va_start(valist, n);
 	sum = 0;
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
 
 	va_end(valist);
