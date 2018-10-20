770cfa9e85aefb23f732bbcdd7f82a7c70dfd3d6
Martin Smith <538@holbertonschool.com>
2018-10-20 21:43:57 +0000

    modify 2 and stype fix

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -12,20 +12,21 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list vl;
+	va_list vls;
 	unsigned int i;
 
-	va_start(vl, n);
+	va_start(vls, n);
 
 	if (n == 0)
 		return;
 	i = 0;
 	while (i < n)
 	{
-		printf("%s", va_arg(vl, char*));
+		printf("%s", va_arg(vls, char*));
 		i++;
 		if (i < n && separator)
 			printf("%s", separator);
 	}
 	putchar(10);
+	va_end(vls);
 }
