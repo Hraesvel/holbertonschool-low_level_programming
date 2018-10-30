45c7242ad5827b884a2a2717cc224ebeb9613061
Martin Smith <538@holbertonschool.com>
2018-10-30 20:53:18 +0000

    Hotfix task 100 * Added - check is only 1 element.

--- 0x12-more_singly_linked_lists/100-reverse_listint.c
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -11,8 +11,15 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *temp, *pre;
 
-	if (!(*head))
+	if (!head || !(*head))
+	{
 		return (NULL);
+	}
+
+	if (!(*head)->next)
+	{
+		return (*head);
+	}
 	temp = *head;
 	pre = NULL;
 	while (1)
