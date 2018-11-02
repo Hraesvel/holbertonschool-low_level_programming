392704bd46cfba0d3cd096f1068c94393d819744
Martin Smith <538@holbertonschool.com>
2018-11-02 03:30:58 +0000

    Modify task 3
    Remove len function, not needed\n add a case for max bit count for unsigned long int

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,19 +1,4 @@
 /**
- * bin_len - return length of the binary rep of a number.
- * @n: number to count the binary length
- *
- * Return: length.
- */
-unsigned int bin_len(unsigned long int n)
-{
-	unsigned int i;
-
-	for (i = 0; n != 0; ++i)
-		n >>= 1;
-	return (i);
-}
-
-/**
  * set_bit - function that sets the value of a bit to 1 at a given index.
  * @n: pointer to a number
  * @index:
@@ -22,9 +7,8 @@ unsigned int bin_len(unsigned long int n)
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (bin_len(*n) - 1 < index)
+	if (index > 64)
 		return (-1);
-
 	if (!((*n >> index) & 1))
 	{
 		*n += 1 << index;
