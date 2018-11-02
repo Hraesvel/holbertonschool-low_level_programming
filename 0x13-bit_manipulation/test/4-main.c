84c2757412ced7e6f40b6349c99940ce04f8eb82
Martin Smith <538@holbertonschool.com>
2018-11-02 05:13:55 +0000

    clean up

--- 0x13-bit_manipulation/test/4-main.c
+++ 0x13-bit_manipulation/test/4-main.c
@@ -13,23 +13,23 @@ int main(void)
 	
 	n = 1024;
 	ret = clear_bit(&n, 10);
+	printf("ret %d: ", ret);
 	printf("%lu\n", n);
-	printf("ret %d\n", ret);
 
 	n = 0;
 	ret= clear_bit(&n, 10);
+	printf("ret %d: ", ret);
 	printf("%lu\n", n);
-	printf("ret %d\n", ret);
 	
 	n = 98;
 	ret = clear_bit(&n, 1);
+	printf("ret %d: ", ret);
 	printf("%lu\n", n);
-	printf("ret %d\n", ret);
 	
 	n = 1024;
 	ret = clear_bit(&n, 9);
+	printf("ret %d: ", ret);
 	printf("%lu\n", n);
-	printf("ret %d\n", ret);
 	
 	return (0);
 }
