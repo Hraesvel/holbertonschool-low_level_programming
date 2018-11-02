a69c189ffc6f59a2a9e1a4c92636cca50afc7a74
Martin Smith <538@holbertonschool.com>
2018-11-02 03:46:01 +0000

    Update task 4

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -1,19 +1,4 @@
 /**
- * bin_len - return length of the binary rep of a number.
- * @n: number to count the binary length
- *
- * Return: length.
- */
-unsigned int bin_len(unsigned long int n)
-{
-	unsigned int i;
-
-	for (i = 0; n != 0; ++i)
-		n >>= 1;
-	return (i);
-}
-
-/**
  * clear_bit- function that sets the value of a bit to 0 at a given index.
  * @n: pointer to a number
  * @index: where to set the bit to 0
@@ -22,13 +7,12 @@ unsigned int bin_len(unsigned long int n)
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (bin_len(*n) - 1 < index)
+	if (index > 64)
 		return (-1);
-
 	if ((*n >> index) & 1)
 	{
 		*n -= 1 << index;
-		return (0);
+		return (1);
 	}
-	return (-1);
+	return (1);
 }
