b881b5a95ea006213035fa693aa1a5307e15392d
Martin Smith <538@holbertonschool.com>
2018-11-02 01:08:46 +0000

    T5: style fix

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 /**
  * flip_bits - function that returns the number of bits you would need to flip
  * to get from one number to another.
- * @n: starting 
+ * @n: starting
  * @m: target
  *
  * Return: number bits flipped
@@ -11,8 +11,8 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int bits = 0;
 	unsigned long int xor;
 
-	xor = n^m;
-	
+	xor = n ^ m;
+
 	while (xor)
 	{
 		bits += xor & 1;
