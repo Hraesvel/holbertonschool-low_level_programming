2adc840a999c647fc31db2a1786881a23e1f49c4
Martin Smith <538@holbertonschool.com>
2018-11-02 04:20:17 +0000

    Recoded Task 0
    Using only one loop and bitwise operator to convert string to number

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -9,24 +9,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num, i, expo = 1;
+	unsigned int num, i;
 
 	if (!b || !*b)
 		return (0);
 
-	i = 0;
+	num = i = 0;
 	while (b[i])
 	{
-		if (b[i] - 48 > 1)
+		if (b[i] > 49)
 			return (0);
+		else if (b[i] == 49)
+		{
+			num <<= 1;
+			num += 1;
+		}
+		else
+			num <<= 1;
 		i++;
 	}
-	i--;
-	num = 0;
-	do {
-		num += (b[i] - '0') * expo;
-		expo *= 2;
-	} while (i--);
-
 	return (num);
 }
