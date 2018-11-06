71e868dca0d99a8999c3b9d4ab21131d1af8e9e2
Martin Smith <538@holbertonschool.com>
2018-11-06 21:40:20 +0000

    adding task 0

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - fuction that read a text file and print it out the POSIX std
+ * out
+ * @filename: body of text to print.
+ * @letters: max char to print.
+ * Return: number of chars printed.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int len, fd, err;
+	char *buf;
+
+	len = fd = err = 0;
+	if (!filename)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters + 1);
+	if (!buf)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	err = read(fd, buf, letters);
+	if (err < 0)
+		return (0);
+
+	buf[letters + 1] = '\0';
+
+	while (buf[len])
+		len++;
+	err = write(1, buf, len);
+	if (err <= 0)
+		return (0);
+
+	free(buf);
+	close(fd);
+	return (len);
+}
