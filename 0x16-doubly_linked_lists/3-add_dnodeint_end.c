78edbf06c8a82fbedbc749f16cfa14fd5a8d4625
Martin smith <msmith3dart@icloud.com>
2018-12-09 20:13:05 -0800

    hotfix task 3

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,19 +12,24 @@ static dlistint_t *new_node(const int n);
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *node;
+	dlistint_t *node, *new;
 
 	if (*head)
 	{
 		node = *head;
 		while (node->next)
 			node = node->next;
-		node->next = new_node(n);
+		new = new_node(n);
+		if (!new)
+			return (NULL);
+		node->next = new;
 		node->next->prev = node;
 	}
 	else
 	{
 		node = new_node(n);
+		if (!node)
+			return (NULL);
 		*head = node;
 	}
 	return (node);
