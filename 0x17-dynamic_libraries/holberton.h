3e2a4c30711787727d558bc6f1673fde7cb2d98f
Martin smith <msmith3dart@icloud.com>
2018-12-10 13:52:05 -0800

    updated

--- 0x17-dynamic_libraries/holberton.h
+++ 0x17-dynamic_libraries/holberton.h
@@ -76,4 +76,9 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+
+/* Prototype 0x017 */
+
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
 #endif /* _HOLBERTON_H_ */
