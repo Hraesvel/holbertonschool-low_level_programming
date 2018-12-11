2e51091aaa457b6301dee143d506f591036bdfb6
Martin Smith <38019643+Ostoyae@users.noreply.github.com>
2018-12-10 16:10:43 -0800

    Update 1-create_dynamic_lib.sh

--- 0x17-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x17-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 gcc -fPIC -Wall -Werror -Wextra -pedantic -c *.c
-gcc -Wall -Werror -Wextra -pedantic --shared *.o -o liball.so
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -shared *.o -o liball.so
