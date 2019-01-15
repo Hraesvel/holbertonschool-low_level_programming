8c1e4e86c9cdeebf478dde786c0e401f58604875
Martin Smith <msmith3dart@icloud.com>
2019-01-15 01:40:14 +0000

    print all nodes

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -6,24 +6,30 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *node;
 
 	idx = i = 0;
-
+	
 	printf("{");
 	while (idx < ht->size)
 	{
 		node = ht->array[idx];
-		if (ht->array[idx++])
+		while (node)
 		{
 			printf("\'%s\': \'%s\'",node->key, node->value);
-			i = idx + 1;
-			while (i < ht->size)
-			{
-				if (ht->array[i++])
-				{
-					printf(", ");
-					break;
-				}
-			}
+			
+			if (node->next)
+				printf(", ");
+			else
+				idx++;
+			node = node->next;
 		}
+		
+		while (!(ht->array[idx + 1]))
+			idx++;
+
+		if (i && idx < ht->size)
+ 			printf(", ");
+		else
+			i = 1;
+		idx++;
 	}
 	printf("}\n");
 }
